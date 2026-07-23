c901ea18d2dd4a0dff44fe94ec3acd448bd488c4
Henry Alvarez Astudillo <halvarezast@gmail.com>
2026-07-23 08:41:12 -0500

    UpdateCFA

--- src/config/global.js
+++ src/config/global.js
@@ -267,7 +267,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/72312206_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
